3722ea268fe6eab337c0022ea2be784190277ebb
Maciek Turek <raysten87@gmail.com>
2019-04-22 20:29:12 +0200

    Working on passive income.

---
+++ Assets/Scripts/PassiveInccome/PassiveIncomeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class PassiveIncomeController : MonoBehaviour, IIncomeMaker
+{
+	private IMoneyStorage moneyStorage;
+	private float incomeRate;
+	private float interval; // TODO:
+
+	[Inject]
+	public void Construct(IMoneyStorage moneyStorage)
+	{
+		this.moneyStorage = moneyStorage;
+	}
+
+	public float GetIncomeRate()
+	{
+		return incomeRate;
+	}
+
+	public void SetIncomeRate(float incomeRate)
+	{
+		this.incomeRate = incomeRate;
+	}
+}
---
+++ Assets/Scripts/PassiveInccome/PassiveIncomeSettings.cs
@@ -0,0 +1,11 @@
+﻿public class PassiveIncomeSettings
+{
+	public float InitialIncomeRate;
+	public float InitialInterval;
+	public float IncomeUpgradeFactor;
+	public float IncomeUpgradeCostChange;
+	public float InitialIntervalUpgradeCost;
+	public float IntervalUpgradeCostChange;
+	public float[] IntervalUpgradeChanges;
+	public int MaximumIncomeUpgradeCount;
+}
