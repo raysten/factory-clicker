30acba7ec3051411229fb2662ede98941857c794
Maciek Turek <raysten87@gmail.com>
2019-04-24 21:09:47 +0200

    Working on special events.

--- Assets/Scripts/Installers/GameSettingsInstaller.cs
+++ Assets/Scripts/Installers/GameSettingsInstaller.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using Zenject;
+using System.Collections.Generic;
 
 [CreateAssetMenu(menuName = "Factory/Game Settings")]
 public class GameSettingsInstaller : ScriptableObjectInstaller<GameSettingsInstaller>
 {
 	public ActiveIncomeSettings ActiveIncomeSettings;
 	public PassiveIncomeSettings PassiveIncomeSettings;
+	public List<EventData> SpecialEvents;
 
 	public override void InstallBindings()
 	{
 		Container.BindInstance(ActiveIncomeSettings);
 		Container.BindInstance(PassiveIncomeSettings);
+		Container.BindInstance(SpecialEvents); // TODO: Bind list?
 	}
 }
---
+++ Assets/Scripts/SpecialEvents/EventData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Factory/SpecialEvents")]
+public class EventData : ScriptableObject
+{
+	public string Title;
+	public string Description;
+	public float Probability; // In percents, % chance of an event happening during 1 second period.
+	public List<EventOption> Options;
+
+	[Serializable]
+	public class EventOption
+	{
+		public string OptionName;
+		public List<EventOutcome> Outcomes;
+	}
+
+	[Serializable]
+	public class EventOutcome
+	{
+		public float Probability;
+		public float PercentageMoneyChange;
+		public float AbsoluteMoneyChange;
+		public string Description;
+	}
+}
