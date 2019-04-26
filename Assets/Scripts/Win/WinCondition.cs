24fbaeca576970a73c783332608560b7ab7c082f
Maciek Turek <raysten87@gmail.com>
2019-04-26 22:25:34 +0200

    Initial win condition handling.

--- Assets/Scripts/Win/WinCondition.cs
+++ Assets/Scripts/Win/WinCondition.cs
@@ -7,12 +7,19 @@ public class WinCondition : IInitializable, IDisposable
 	private SignalBus signalBus;
 	private float winAmount;
 	private readonly HighscoreController highscoreController;
+	private readonly WinPanel winPanel;
 
-    public WinCondition(SignalBus signalBus, WinSettings settings, HighscoreController highscoreController)
+	public WinCondition(
+		SignalBus signalBus,
+		WinSettings settings,
+		HighscoreController highscoreController,
+		WinPanel winPanel
+	)
 	{
 		this.signalBus = signalBus;
 		this.winAmount = settings.WinAmount;
 		this.highscoreController = highscoreController;
+		this.winPanel = winPanel;
 	}
 
 	public void Initialize()
@@ -29,9 +36,9 @@ public class WinCondition : IInitializable, IDisposable
 	{
 		if (balanceChangedInfo.Amount >= winAmount)
 		{
-			// TODO: End game.
 			Time.timeScale = 0f; // TODO:
 			highscoreController.OnGameWon();
+			winPanel.Show();
 		}
 	}
 }
---
+++ Assets/Scripts/Win/WinPanel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WinPanel : MonoBehaviour
+{
+    public void Show()
+	{
+		gameObject.SetActive(true);
+	}
+
+	public void Close()
+	{
+		// TODO: Load menu scene.
+	}
+}
